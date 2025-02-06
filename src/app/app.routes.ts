1650e8a28f894d86b6efc930f2dff7b559163578
istiak.ahammad <l@6313..>
2025-02-06 23:14:05 +0600

    necessary change to add a new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import {StockInComponent} from './serviceItem/stock-in/stock-in.component';
 import {PurchaseOrderComponent} from './serviceItem/purchase-order/purchase-order.component';
 import {HomeComponent} from './serviceItem/home/home.component';
 import {AddItemComponent} from './serviceItem/item/add-item/add-item.component';
+import {UserDetailsComponent} from './serviceItem/user-details/user-details.component';
 
 
 export const routes: Routes = [
@@ -15,8 +16,13 @@ export const routes: Routes = [
   {
     path: 'home', component: HomeComponent,
   },
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
-    path: 'stockOut', component: AddItemComponent
+    path: '', redirectTo: '/home', pathMatch: 'full'
+  },
+  {
+    path: 'add-item', component: AddItemComponent
+  },
+  {
+    path: 'user', component: UserDetailsComponent
   },
 ];
--- src/app/serviceItem/item/add-item/add-item.component.ts
+++ src/app/serviceItem/item/add-item/add-item.component.ts
@@ -3,13 +3,13 @@ import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
 import {MatInput, MatInputModule} from '@angular/material/input';
 import {MatSelectModule} from '@angular/material/select';
 import {MatButton} from '@angular/material/button';
-import {NgForOf} from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-add-item',
-  imports: [MatFormFieldModule, MatInputModule,MatButtonModule, MatSelectModule, MatRadioGroup, MatRadioButton, MatButton],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatSelectModule, MatRadioGroup, MatRadioButton, RouterOutlet],
   templateUrl: './add-item.component.html',
   styleUrl: './add-item.component.css'
 })
