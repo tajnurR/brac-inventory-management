174d4422f33095b839831ce2fa256a3e5dd73728
istiak.ahammad <l@6313..>
2025-02-10 19:00:51 +0600

    updated side-bar design

--- src/app/common/right-side-bar/right-side-bar.component.ts
+++ src/app/common/right-side-bar/right-side-bar.component.ts
@@ -19,23 +19,40 @@ export class RightSideBarComponent {
 
   isStockInExpanded = false;
 
+  // Track the currently expanded menu item
+  expandedItem: string | null = null;
+
   navItems = [
     { link: '/home', label: 'Dashboard', icon: 'home' },
     { link: '/order', label: 'Purchase Order', icon: 'shopping_cart' },
     {
       link: '/stock',
       label: 'Stock In',
-      icon: 'inventory',
+      icon: 'login',
       subItems: [
         { link: '/stock/sub-item-1', label: 'Sub Item 1', icon: 'list' },
         { link: '/stock/sub-item-2', label: 'Sub Item 2', icon: 'list' },
         { link: '/stock/sub-item-3', label: 'Sub Item 3', icon: 'list' },
       ],
     },
-    { link: '/item-add', label: 'Item Add', icon: 'inventory' },
     { link: '/stock-out', label: 'Stock Out', icon: 'logout' },
     { link: '/history', label: 'Stock History', icon: 'history' },
-    { link: '/setup', label: 'Master Setup', icon: 'settings' },
+    { link: '/setup', label: 'Master Setup', icon: 'settings',
+      subItems: [
+        { link: 'setup/category-list', label: 'Category', icon: 'inbox' },
+        { link: '/item-add', label: 'Item', icon: 'category' },
+        { link: 'setup/item-group-list', label: 'Item Group', icon: 'list' },
+        { link: 'setup/uom-list', label: 'UoM', icon: 'ac_unit' },
+        { link: 'setup/organization-list', label: 'Organization', icon: 'corporate_fare' },
+      ],},
+    { link: '/uam', label: 'UAM', icon: 'group_add',
+      subItems: [
+        { link: 'user-details', label: 'User Details', icon: 'person_raised_hand' },
+        // { link: '/item-add', label: 'Item', icon: 'category' },
+        // { link: 'setup/item-group-list', label: 'Item Group', icon: 'list' },
+        // { link: 'setup/uom-list', label: 'UoM', icon: 'ac_unit' },
+        // { link: 'setup/organization-list', label: 'Organization', icon: 'corporate_fare' },
+      ],},
     { link: '/reports', label: 'Report Manager', icon: 'assessment' },
   ];
 
@@ -56,8 +73,17 @@ export class RightSideBarComponent {
     }
   }
 
-  toggleStockInSubMenu() {
-    this.isStockInExpanded = !this.isStockInExpanded;
+  // Toggle the expanded state of a menu item
+  toggleSubMenu(link: string) {
+    if (this.expandedItem === link) {
+      this.expandedItem = null; // Collapse if already expanded
+    } else {
+      this.expandedItem = link; // Expand the clicked item
+    }
   }
 
+  // Check if a menu item is expanded
+  isItemExpanded(link: string): boolean {
+    return this.expandedItem === link;
+  }
 }
