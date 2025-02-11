6565e4947bd465133b1f92c5620f06ee2891e8ac
istiak.ahammad <l@6313..>
2025-02-11 14:40:44 +0600

    ADD UaM components

---
+++ src/app/serviceItem/roleUAM/add-role/add-role.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-role',
+  imports: [],
+  templateUrl: './add-role.component.html',
+  styleUrl: './add-role.component.css'
+})
+export class AddRoleComponent {
+
+}
