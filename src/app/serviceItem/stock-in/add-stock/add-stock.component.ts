faa5cc8dbb2544070cd4c7e4017fce19e4f5fe08
istiak.ahammad <l@6313..>
2025-02-13 18:04:12 +0600

    .

--- src/app/serviceItem/stock-in/add-stock/add-stock.component.ts
+++ src/app/serviceItem/stock-in/add-stock/add-stock.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import {MatRadioButton, MatRadioGroup} from '@angular/material/radio';
 import {FormsModule} from '@angular/forms';
 import {NgComponentOutlet, NgForOf, NgIf} from '@angular/common';
-import {AddRoleComponent} from '../../roleUAM/add-role/add-role.component';
-import {UomListComponent} from '../../uom/uom-list/uom-list.component';
 import {RodComponent} from './rod/rod.component';
 import {GiftComponent} from './gift/gift.component';
 import {PoReceiveComponent} from './po-receive/po-receive.component';
@@ -38,15 +36,17 @@ export class AddStockComponent {
     }
   }
 
-  files: File[] = []; // Array to store all selected files
+  files: File[] = [];
 
-  // Method to handle file selection
   onFileSelected(event: Event) {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
-      // Append new files to the existing `files` array
       this.files = [...this.files, ...Array.from(input.files)];
       console.log('All Files:', this.files); // Debugging
     }
+  }
+
+  removeFile(index: number) {
+    this.files.splice(index, 1); // Remove the file at the specified index
   }
 }
